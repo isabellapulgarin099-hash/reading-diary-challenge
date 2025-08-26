6fed42c362754c018991fef815afde8c1cdbdf71
hector espinal pelaez <tuinsano98@icloud.com>
2025-08-25 19:45:56 -0500

    Verifica si hay notas. Si la lista de notas está vacía (self.notes), entonces retorna -1.
    Cuenta cuántas notas hay por página.
    Usa un diccionario note_count donde la clave es el número de página y el valor es el número de notas en esa página.
    
    Encuentra la página con más notas.
    Usa max(..., key=...) para encontrar la clave (página) con el valor más alto.

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -1,3 +1,39 @@
 from datetime import datetime
 
-# TODO: Add code here
+class Note:
+    def __init__(self, text, page, date):
+        self.text = text
+        self.page = page
+        self.date = date
+
+    def __str__(self):
+        return f"{self.date} - página {self.page}: {self.text}"
+
+class Book:
+    EXCELLENT = 3
+    GOOD = 2
+    BAD = 1
+    UNRATED = -1
+
+    def __init__(self, isbn, title, author, pages):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.pages = pages
+        self.rating = Book.UNRATED
+        self.notes = []
+
+    def add_note(self, text, page, date):
+        if page > self.pages:
+            return False
+        note = Note(text, page, date)
+        self.notes.append(note)
+        return True
+
+    def set_rating(self, rating):
+        if rating in (Book.EXCELLENT, Book.GOOD, Book.BAD):
+            self.rating = rating
+            return True
+        return False
+
+
